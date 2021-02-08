6504c319fa5b32f9fb415265a3ccf26666eab573
youneuoy <youneuoy@gmail.com>
2021-02-08 13:14:26 +0300

    implemented ImGui graphics

--- M2TWEOP library/graphicsD3D.h
+++ M2TWEOP library/graphicsD3D.h
@@ -1,6 +1,7 @@
 #pragma once
 #include"headersSTD.h"
 #include "headersMEM.h"
+#include "headersGraphics.h"
 #include "globals.h"
 class graphicsD3D
 {
@@ -11,22 +12,31 @@ public:
 		HWND Window = nullptr;
 
 
+
+		//if imgui initialized
+		bool ImInitialized = false;
+		int ImInitCount = 0;
+		bool ifMouseOrKeyBoardAtImgui = false;
 		struct 
 		{
 			WNDPROC oWndProc = NULL;
 			// Originals
-			uint64_t onewGameWndProc = NULL;
-			uint64_t oEndScene = NULL;
-			uint64_t oReset = NULL;
+			uint32_t onewGameWndProc = NULL;
+			uint32_t oEndScene = NULL;
+			uint32_t oReset = NULL;
 		}hookD;
 	}dataS;
+	static NOINLINE void APIENTRY Draw(LPDIRECT3DDEVICE9 pDevice);
+
+
 
 	static NOINLINE LRESULT APIENTRY hkWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 	static NOINLINE LRESULT APIENTRY hkWndProc2(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 	static NOINLINE HRESULT APIENTRY hkEndScene(IDirect3DDevice9* pDevice);
+
 	static NOINLINE HRESULT APIENTRY hkReset(IDirect3DDevice9* pDevice, D3DPRESENT_PARAMETERS* pPresentationParameters);
 
-
+	static NOINLINE void APIENTRY initImgGui(IDirect3DDevice9* pDevice);
 	static bool init();
 	static DWORD WINAPI InitS();
 	static bool GetD3D9Device(void** pTable, size_t Size);
