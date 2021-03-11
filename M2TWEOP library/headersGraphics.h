472fb315cdb9918ce24bc0ef5bf47ec8abc5a2b9
youneuoy <youneuoy@gmail.com>
2021-03-11 20:21:21 +0300

    fixed problems with displaying menus on the tactical map: victory-style: Fixed crashes when playing with shadows enabled.
    Launcher development started

--- M2TWEOP library/headersGraphics.h
+++ M2TWEOP library/headersGraphics.h
@@ -1,7 +1,7 @@
 #pragma once
-#include "imgui/imgui.h"
-#include "imgui/imgui_impl_win32.h"
-#include "imgui/imgui_impl_dx9.h"
+#include "imgui.h"
+#include "imgui_impl_win32.h"
+#include "imgui_impl_dx9.h"
 #include "imgui_internal.h"
 
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
