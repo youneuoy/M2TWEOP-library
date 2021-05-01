2dbec7c1d5dfb6611146c18e1df665db59fe0271
youneuoy <youneuoy@gmail.com>
2021-05-02 01:08:56 +0300

    rebuild some libraries, add them to project folder

--- M2TWEOP library/headersMEM.h
+++ M2TWEOP library/headersMEM.h
@@ -1,4 +1,10 @@
 #pragma once
+#if _DEBUG
 
+#pragma comment(lib,"libMinHook.x86D.lib")
+#else
+#pragma comment(lib,"libMinHook.x86.lib")
+
+#endif
 #include "MinHook.h"
 #define NOINLINE __declspec(noinline)
