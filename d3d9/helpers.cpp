43ee6f6a16460ef9ef9958d5e627461b566d0fdd
youneuoy <youneuoy@gmail.com>
2021-05-08 00:36:04 +0300

    finally fixed game launch. Maybe.

--- d3d9/helpers.cpp
+++ d3d9/helpers.cpp
@@ -21,53 +21,41 @@ vector<std::string> helpers::splitString(const std::string& phrase, const std::s
 #include <boost/interprocess/managed_shared_memory.hpp>
 void helpers::doEOPPipe(std::string& result, int waitSeconds)
 {
-    Sleep(waitSeconds * 1000);
-    using namespace boost::interprocess;
-    //boost way ;(
-    try
-    {
-        int correctResponce = 0;
-        //Open already created shared memory object.
-        shared_memory_object shm(open_only, "M2TWEOPStartMem", read_write);
+    ULONGLONG endTime = GetTickCount64() + 1000ull * waitSeconds;
+    namespace bip = boost::interprocess;
 
-        //Map the whole shared memory in this process
-        mapped_region region(shm, read_write);
-
-        //Check that memory was initialized to 1
-        char* mem = static_cast<char*>(region.get_address());
-        int strSize = *mem;
-
-        std::string buf;
-        buf.resize(strSize);
-        //example of the function that can write to a buffer with 10-100 characters.
-        memcpy(&buf[0], mem + sizeof(strSize), strSize);
-        buf.resize(strSize);
-        buf.shrink_to_fit();
-
-        result = buf;
-        *mem = correctResponce;
-
-        region.flush();
-
-        /*
-        typedef boost::interprocess::allocator<char, boost::interprocess::managed_shared_memory::segment_manager> CharAllocator;
-        typedef boost::interprocess::basic_string<char, std::char_traits<char>, CharAllocator> ourS;
-
-        //Open already created shared memory object.
-        managed_shared_memory shm(open_only, "M2TWEOPStartPipe",0,);
-
-        std::pair<ourS*, std::size_t> ret = shm.find_no_lock<ourS>("EOPStr");
-        if (ret.first != nullptr && ret.second!=0)
+    do {
+        //boost way ;(
+        try
         {
-            result = (ret.first->c_str());
-            shm.destroy<ourS>("EOPStr");
+            //Open already created shared memory object.
+            bip::shared_memory_object shm(bip::open_only, "M2TWEOPStartMem1", bip::read_write);
+
+
+            //Map the whole shared memory in this process
+            bip::mapped_region region(shm, bip::read_write);
+
+            //Check that memory was initialized to 1
+            char* mem = static_cast<char*>(region.get_address());
+            int strSize = *mem;
+
+            std::string buf;
+            buf.resize(strSize);
+            //example of the function that can write to a buffer with 10-100 characters.
+            memcpy(&buf[0], mem + sizeof(strSize), strSize);
+            buf.resize(strSize);
+            buf.shrink_to_fit();
+
+            result = buf;
+            *mem = 0;
+
+            region.flush();
+
         }
-        */
+        catch (...)
+        {
 
+        }
 
-    }
-    catch (...)
-    {
-    }
-
+    } while (GetTickCount64() < endTime&& result.size()==0);
 }
