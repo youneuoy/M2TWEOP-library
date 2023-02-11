e493ad19cac90e250fb67f86bd5d4f5ebb8dc03e
youneuoy <61095686+youneuoy@users.noreply.github.com>
2023-02-12 00:37:09 +0300

    generateImGuiDocs

---
+++ M2TWEOP DataFiles/youneuoy_Data/plugins/lua/LuaImGuiDocs.lua
@@ -0,0 +1,15 @@
+---Basic ImGui table
+---@class ImGui
+ImGui = { 
+
+}
+
+---Begin window
+---@param name string window name
+---@param open boolean optional
+---@param flags integer optional
+function ImGui.Begin(name, open, flags) end 
+
+---Begin window
+function ImGui.End() end 
+
