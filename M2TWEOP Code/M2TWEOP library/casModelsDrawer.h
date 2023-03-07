1e0b1b4551c4a513f4ee1b1525dbb2b4275818cb
youneuoy <61095686+youneuoy@users.noreply.github.com>
2023-03-07 20:21:05 +0300

    whitespace remove

--- M2TWEOP Code/M2TWEOP library/casModelsDrawer.h
+++ M2TWEOP Code/M2TWEOP library/casModelsDrawer.h
@@ -3,7 +3,7 @@
 #include "exportHeader.h"
 namespace casModelsDrawer
 {
-	EOP_EXPORT void addCasModelToDrawList (UINT32 modelId, int x, int y, float sizeMultiplier);
+	EOP_EXPORT void addCasModelToDrawList(UINT32 modelId, int x, int y, float sizeMultiplier);
 	EOP_EXPORT void removeCasModelFromDrawList(UINT32 modelId);
 
 	void WINAPI drawModels();
