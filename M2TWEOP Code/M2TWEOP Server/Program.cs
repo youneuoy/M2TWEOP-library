679e21619ce6c13023ffc586b2dd1f14507e4f25
youneuoy <61095686+youneuoy@users.noreply.github.com>
2023-03-27 19:18:24 +0300

    template upd

--- M2TWEOP Code/M2TWEOP Server/Program.cs
+++ M2TWEOP Code/M2TWEOP Server/Program.cs
@@ -1,12 +1,56 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace M2TWEOPServer
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+            try
+            {
+                await host.RunAsync();
+            }
+            catch (Exception ex)
+            {
+
+            }
 
         }
     }
+
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("");
+
+            stoppingToken.Register(() => _logger.LogDebug(""));
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("");
+
+
+                await Task.Delay(5000, stoppingToken);
+            }
+        }
+    }
 }
+
