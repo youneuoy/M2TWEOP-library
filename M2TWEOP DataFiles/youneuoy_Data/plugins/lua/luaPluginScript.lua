eff4af62965700d2afe8259f0df79d9d6be66591
Eddie Eldridge <eddie.eldridge@hotmail.com>
2023-05-18 09:49:22 +0100

    Update formatting of luaPluginScript.lua

--- M2TWEOP DataFiles/youneuoy_Data/plugins/lua/luaPluginScript.lua
+++ M2TWEOP DataFiles/youneuoy_Data/plugins/lua/luaPluginScript.lua
@@ -1,50 +1,49 @@
-	require('myconfigs')
-    -- Helper for managing persistence of tables across save/load
-	require('helpers/tableSave')
+require('myconfigs')
 
-    -- Uncomment to use EOP Helper functions
-	-- require('helpers/EopLuaHelpers')
+-- Helper for managing persistence of tables across save/load
+require('helpers/tableSave')
 
-	-- Uncomment to use external debugger
-	-- require('helpers/mobdebug').start()
+-- Uncomment to use EOP Helper functions
+-- require('helpers/EopLuaHelpers')
 
-    --our campaign config table.
-    campaignConfig={["someConfigValue"]=5};
+-- Uncomment to use external debugger
+-- require('helpers/mobdebug').start()
 
-    -- Fires when loading a save file
-    function onLoadSaveFile(paths)
-        campaignPopup=true;
+-- Our campaign config table.
+campaignConfig = { ["someConfigValue"] = 5 };
 
-        for index, path in pairs(paths) do
+-- Fires when loading a save file
+function onLoadSaveFile(paths)
+    campaignPopup = true;
 
-         if (string.find(path, "configTable.lua"))
-         then
-            --function from helper, load saved table
-            campaignConfig=persistence.load(path);
-         end
-
+    for index, path in pairs(paths) do
+        if (string.find(path, "configTable.lua"))
+        then
+            -- Function from helper, load saved table
+            campaignConfig = persistence.load(path);
         end
     end
+end
 
-    -- Fires when creating a save file
-    -- Returns a list of M2TWEOP save files
-    function onCreateSaveFile()
-        local savefiles = {};
-        currentPath=M2TWEOP.getPluginPath();
+-- Fires when creating a save file
+-- Returns a list of M2TWEOP save files
+function onCreateSaveFile()
+    local savefiles = {};
+    currentPath = M2TWEOP.getPluginPath();
 
-        --function from helper, save our table
-        persistence.store(currentPath.."configTable.lua",campaignConfig);
+    -- Function from helper, save our table
+    persistence.store(currentPath .. "configTable.lua", campaignConfig);
 
-        savefiles[1]=currentPath.."configTable.lua";
-        return savefiles;
-    end
+    savefiles[1] = currentPath .. "configTable.lua";
+    return savefiles;
+end
 
-    -- Fires when the plugin is first loaded at game start or reloded with CTRL+9+1
-    function onPluginLoad()
-        M2TWEOP.unlockGameConsoleCommands();
-        -- M2TWEOP.setAncillariesLimit(8);
-        -- M2TWEOP.setMaxBgSize(100);
-        -- M2TWEOP.setReligionsLimit(50);
-        -- M2TWEOP.setBuildingChainLimit(40);
-        -- M2TWEOP.setGuildCooldown(3);
-    end
+-- Fires when the plugin is first loaded at game start or reloded with CTRL+9+1
+function onPluginLoad()
+    M2TWEOP.unlockGameConsoleCommands();
+    -- M2TWEOP.setAncillariesLimit(8);
+    -- M2TWEOP.setMaxBgSize(100);
+    -- M2TWEOP.setReligionsLimit(50);
+    -- M2TWEOP.setBuildingChainLimit(40);
+    -- M2TWEOP.setGuildCooldown(3);
+end
