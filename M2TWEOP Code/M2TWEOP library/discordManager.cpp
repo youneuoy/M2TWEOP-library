229cdff96d6f83ed8c5be1a466ce0c82ad145b2c
Eddie Eldridge <eddie.eldridge@hotmail.com>
2023-06-28 11:41:49 +0100

    Update factionLocalized name parsing

--- M2TWEOP Code/M2TWEOP library/discordManager.cpp
+++ M2TWEOP Code/M2TWEOP library/discordManager.cpp
@@ -4,22 +4,47 @@
 
 namespace discordManager
 {
+	std::string uniStringToStr(UNICODE_STRING **&uniString)
+	{
+		UNICODE_STRING *uniS = *uniString;
+		wchar_t *wstr = (wchar_t *)&uniS->Buffer;
 
-    void OnFactionTurnStart(factionStruct* fac)
-    {
+		std::string strTo;
+		int wchars_num = WideCharToMultiByte(CP_UTF8, 0, wstr, -1, NULL, 0, NULL, NULL);
+		if (wchars_num <= 0)
+		{
+			return strTo;
+		}
+		char *szTo = new char[wchars_num];
+		szTo[wchars_num - 1] = '\0';
+		WideCharToMultiByte(CP_UTF8, 0, wstr, -1, szTo, wchars_num, NULL, NULL);
+
+		strTo = szTo;
+		delete[] szTo;
+
+		return strTo;
+	}
+
+	void OnFactionTurnStart(factionStruct *fac)
+	{
 		if (fac == nullptr)
 		{
 			return;
 		}
 
-		std::string factionName = techFuncs::uniToANSI(fac->localizedName);
+		if (globals::dataS.gameCfg.isDiscordRichPresenceEnabled == true && fac->isPlayerControlled == true && factionName.length() > 0)
+		{
+			UNICODE_STRING **factionName = fac->localizedName;
+			UNICODE_STRING *name = *factionName;
+			if (name->Length == 0)
+			{
+				factionName = (*(*smallFuncs::getFactionName))(fac);
+			}
 
-        if (globals::dataS.gameCfg.isDiscordRichPresenceEnabled == true && fac->isPlayerControlled == true && factionName.length() > 0) {
 			jsonManager::setJSONInFile(
 				"\\youneuoy_Data\\m2tweop_temp\\discordRichPresenceData.json",
 				"factionName",
-				factionName
-			);
+				uniStringToStr(factionName));
 		}
-    }
+	}
 }
