dd5d5ce42ea73019386c7d8df6f0165c8a89fbaa
Medik <steadyeddie101@hotmail.co.uk>
2023-11-26 16:11:49 +0000

    Update doc generation build script

--- documentationGenerator/generateImGuiDocs.ps1
+++ documentationGenerator/generateImGuiDocs.ps1
@@ -1,15 +1,11 @@
-# Colors
-$color =`n" -ForegroundColor Magenta`e[$(35)m"
-$endColor =`n" -ForegroundColor Magenta`e[0m`e[30;"
-
 #remove old lua doc files
-Write-Output "$color ======== 3.1) Remove old documentation ======== $endColor"
+Write-Host "`n`n======== 3.1) Remove old documentation ========`n" -ForegroundColor Magenta
 Remove-item generatedLuaImGuiDocs -recurse  -erroraction 'silentlycontinue'
 
 #create folder for temporary place scripting docs
 new-item ./generatedLuaImGuiDocs -itemtype directory
 
-Write-Output "$color ======== 3.2) Generate documentation config ======== $endColor"
+Write-Host "`n`n======== 3.2) Generate documentation config ========`n" -ForegroundColor Magenta
 
 # Using LuaRocks, call ldoc.lua and pass in the config file
 .\luarocks\lua.exe ./luarocks/rocks/ldoc/1.4.6-2/bin/ldoc.lua . -c configImGui.ld
