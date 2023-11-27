c0bc3192bdf7d65c486de7ba9d73ac4af71e7bf4
FynnTW <fynn.papadopoulos@gmail.com>
2023-11-27 21:51:39 +0200

    fix theme manager

--- M2TWEOP Code/M2TWEOP library/themeManager.h
+++ M2TWEOP Code/M2TWEOP library/themeManager.h
@@ -3,13 +3,6 @@
 #include "headersSTD.h"
 #include "imgui.h"
 
-// Convert RGBA strings to ImVec4
-auto parseColor = [](const std::string &colorStr) -> ImVec4
-{
-    ImVec4 color;
-    sscanf_s(colorStr.c_str(), "rgba(%f, %f, %f, %f)", &color.x, &color.y, &color.z, &color.w);
-    return color;
-};
 
 void tomlToStyle(std::string themeName);
 
