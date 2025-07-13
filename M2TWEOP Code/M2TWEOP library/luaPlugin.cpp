866d0da97b4cf4def16f978dea626dbc593e24ae
Medik <steadyeddie101@hotmail.co.uk>
2025-07-13 16:27:42 +0100

    Revert "Add viewBattleMap"

--- M2TWEOP Code/M2TWEOP library/luaPlugin.cpp
+++ M2TWEOP Code/M2TWEOP library/luaPlugin.cpp
@@ -531,19 +531,6 @@ sol::state* luaPlugin::init(std::string& luaFilePath, std::string& modPath)
 	tables.M2TWEOP.set_function("setAncillariesLimit", &gameHelpers::setAncLimit);
 
 	/***
-	TODO: Port this to a lua command instead of being hardcoded. Current implementation crashes.
-	Displays the battle map at the specified coordinates. Functions the same as the MMB button viewer.
-	@function M2TWEOP.viewBattleMap(x,y)
-	@tparam int x 
-	@tparam int y
-	@usage
-	M2TWEOP.viewBattleMap(100,200);
-	local sett = CAMPAIGN:getSettlement("Rome");
-	M2TWEOP.viewBattleMap(sett.xCoord, sett.yCoord);
-	*/
-	// tables.M2TWEOP.set_function("viewBattleMap", &tacticalMapViewer::view);
-
-	/***
 	Unlocks all console commands, also allows the use of the 'control' command to change factions in singleplayer campaigns.
 	@function M2TWEOP.unlockGameConsoleCommands
 	@usage
